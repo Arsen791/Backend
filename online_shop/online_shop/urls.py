db318de43c467ff94a3ba55c082e30206098e9ae
Arsen791 <114273777+Arsen791@users.noreply.github.com>
2023-02-14 10:40:45 +0600

    test

--- online_shop/online_shop/urls.py
+++ online_shop/online_shop/urls.py
@@ -1,4 +1,4 @@
-"""lecture_3 URL Configuration
+"""online_shop URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
